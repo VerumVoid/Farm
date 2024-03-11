e4e9127bbbd536a0f8339f5f432f4781f7628f2f
heger <a@a.com>
2024-03-11 10:02:14 +0100

    Changed Dandelion

--- src/Flowers/Dandelion.java
+++ src/Flowers/Dandelion.java
@@ -3,5 +3,10 @@ package Flowers;
 import General.Flower;
 
 public class Dandelion extends Flower {
-
+    public Dandelion() {
+        name = "dandelion";
+        price = 5;
+        neededArea = 5;
+        chanceOfGrowth = 0.1;
+    }
 }
