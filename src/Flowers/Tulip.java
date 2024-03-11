88a01ddff60690ad37d390ca9f80ef48f83edf1d
heger <a@a.com>
2024-03-11 10:00:07 +0100

    Added tulip

--- src/Flowers/Tulip.java
+++ src/Flowers/Tulip.java
@@ -3,4 +3,10 @@ package Flowers;
 import General.Flower;
 
 public class Tulip extends Flower {
+    public Tulip() {
+        name = "tulip";
+        price = 5;
+        neededArea = 5;
+        chanceOfGrowth = 0.5;
+    }
 }
